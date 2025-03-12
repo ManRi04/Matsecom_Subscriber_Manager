0793e8c38967ea90b0ed52ea2cd99bad5050b830
ManRi04 <mriessz04@gmail.com>
2025-03-12 15:52:11 +0100

    Class that creates table

--- src/main/java/org/example/CreateTable.java
+++ src/main/java/org/example/CreateTable.java
@@ -1,29 +1,31 @@
 package org.example;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class CreateTable {
-    public static void createNewTable() {
-        String sql = "CREATE TABLE IF NOT EXISTS users ("
-                + "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + "name TEXT NOT NULL, "
-                + "age INTEGER);";
 
-        try (Connection conn = SQLiteConnection.connect()) {
-            assert conn != null;
-            try (Statement stmt = conn.createStatement()) {
-                stmt.execute(sql);
-                System.out.println("Tabelle erstellt oder bereits vorhanden.");
-            }
+public class CreateTable {
+    public static void main(String[] args) {
+        // SQLite connection string
+        var url = "jdbc:sqlite:src/main/java/org/example/database2.db";
+
+        // SQL statement for creating a new table
+        var sql = "CREATE TABLE IF NOT EXISTS warehouses ("
+                + "	id INTEGER PRIMARY KEY,"
+                + "	name text NOT NULL,"
+                + "	capacity REAL"
+                + ");";
+
+        try (var conn = DriverManager.getConnection(url);
+             var stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
-
-    public static void main(String[] args) {
-        createNewTable();
-    }
 }
+
 
