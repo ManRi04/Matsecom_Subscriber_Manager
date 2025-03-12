a0ac66492e5ba3acbebdd68101c1cdf161bd66b3
ManRi04 <mriessz04@gmail.com>
2025-03-12 16:08:29 +0100

    creation of warehouses successful

--- src/main/java/org/example/CreateTable.java
+++ src/main/java/org/example/CreateTable.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 public class CreateTable {
     public static void main(String[] args) {
         // SQLite connection string
-        var url = "jdbc:sqlite:src/main/java/org/example/database2.db";
+        var url = "jdbc:sqlite:src/main/java/org/example/database.db";
 
         // SQL statement for creating a new table
         var sql = "CREATE TABLE IF NOT EXISTS warehouses ("
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class User {
+    String prename;
+    String surname;
+    String IMSI;
+    String terminal_type;
+    String subscription_type;
+
+    public void create(){
+
+    }
+
+}
